9ddd54e2b0c81e49a65b781cafa2058926d3730b
apriljsu <apriljsu@gmail.com>
2022-07-06 22:56:53 -0400

    added css keyframe and bordem function

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
-//1)Create an Object or Class using hunger,Sleepiness,boredom and age
+//create button variables
+
+//Create an Object or Class using hunger,Sleepiness,boredom and age
 class pet {
   constructor(name,age,hunger,boredom,sleepiness){
     this.name=name
@@ -7,9 +9,9 @@ class pet {
     this.boredom=boredom
     this.sleepiness=sleepiness
   }
-  //2) create functions for age, eat,hunger,boredom to decrement
+  //create functions for age, eat,hunger,boredom to decrement
   feed(){
-    this.hunger=this.hunger-1
+    this.hunger--
   }
   sleep(){
     this.sleepiness--
@@ -19,16 +21,16 @@ class pet {
   }
   }
 
-  //4)Instantiate your Tamagotchi (If you created a class)
+//Instantiate your Tamagotchi (If you created a class)
   const mametchi= new pet ("",0,1,1,1)
-  //add pet name
+//add pet name
   function petName(){
   petName=document.getElementById("petname").value
   document.getElementById("petdes").innerText=`Hi! I am your pet ${petName}!`
   }
-  //add age
+//add age
   function setAge(){
-  setInterval(displayAge,30000)
+  setInterval(displayAge,10000)
   function displayAge(){
     if(mametchi.hunger>=10){
     document.getElementById("age").innerText="your pet has died from starving"
@@ -38,41 +40,57 @@ class pet {
     }
   }
   document.querySelector("#submit").addEventListener("click",setAge)
-  // add Hunger
+// add Hunger display and event listener
   function setHunger(){
     setInterval(displayHunger,2000)
     function displayHunger(){
       if(mametchi.hunger>=10){
         document.getElementById("hunger").innerText="your pet has died from starving"
       }else{
-        document.getElementById("hunger").innerText=mametchi.hunger ++
+        document.getElementById("hunger").innerText=mametchi.hunger++
       }
     }
   }
   document.querySelector("#submit").addEventListener("click",setHunger)
-//add event listeners to feed button
-document.getElementById("feed").addEventListener("click",(e)=>{
+//add event listener to feed button
+  document.getElementById("feed").addEventListener("click",(e)=>{
   mametchi.feed()
   document.getElementById("hunger").innerText=mametchi.hunger
-})
+  })
+// add boredom display and event listener
+  function setBoredom(){
+    setInterval(displayBoredom,1500)
+    function displayBoredom(){
+      if(mametchi.boredom>=10){
+        document.getElementById("boredom").innerText="your pet has died from boredom"
+      }else{
+        document.getElementById("boredom").innerText=mametchi.boredom++
+      }
+    }
+  }
+  document.querySelector("#submit").addEventListener("click",setBoredom)
+//add event listener to play button
+  document.getElementById("play").addEventListener("click",(e)=>{
+    mametchi.play()
+    document.getElementById("play").innerText=mametchi.boredom
+  })
+//add sleep display and event listener
+function setSleep(){
+  setInterval(displaySleep, 2500)
+  function displaySleep(){
+    if(mametchi.sleep>=10){
+      document.getElementById("sleepiness").innerText="your pet has died from sleep deprivation"
+    }else{
+      document.getElementById("sleepiness").innerText=mametchi.sleepiness++
+    }
+  }
+}
+document.querySelector("#submit").addEventListener("click",setSleep)
+//add event listeners to sleep button
+// document.getElementById("sleep").addEventListener(click)
 
 
-  // document.getElementById("hunger").innerText=mametchi.hunger
-  // document.getElementById("boredom").innerText=mametchi.boredom
-  // document.getElementById("sleepiness").innerText=mametchi.sleepiness
-  // document.getElementById("submit").addEventListener("click",petName())
-  // Increase your pet's age every x minutes
-  //
-  // Increase your pet's Hunger, Sleepiness, and Bored metrics on an interval of your choosing.
 
-
-// Add buttons to the screen to feed your pet, turn off the lights, and play with your pet.
-//
-// Increase your pet's age every x minutes
-//
-// Increase your pet's Hunger, Sleepiness, and Bored metrics on an interval of your choosing.
-//
-// You pet should die if Hunger, Boredom, or Sleepiness hits 10.
 //
 // Morph your pet at certain ages.
 //
